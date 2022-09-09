411b80ea2ff2285b1314e9661171334a4fc0bbcb
Adrian Acurero <105818616+Adr-AA@users.noreply.github.com>
2022-09-09 11:15:00 -0400

    Upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const numbers = [1, 2, 3, 4, "holaMundo", 5];
+
+//Incrementa los numeros de un array por 1
+function incrementNumbers() {
+    for (let i = 0; i < numbers.length; i++) {
+
+        if (typeof numbers[i] === "number") numbers[i] = numbers[i] + 1;
+    }
+}
+
+incrementNumbers()
+
+console.log(numbers)
