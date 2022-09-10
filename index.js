fcef14dd21d7779ee3a593decfff59a0b9d6db24
Adrian Acurero <105818616+Adr-AA@users.noreply.github.com>
2022-09-09 23:31:47 -0400

    Mejora la funcion incrementNumbers
    Se ha cambiado el for que iteraba

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-const numbers = [1, 2, 3, 4, "holaMundo", 5];
+let numbers = [1, 2, 3, 4, "holaMundo", 5];
 
 //Incrementa los numeros de un array por 1
 function incrementNumbers() {
-    for (let i = 0; i < numbers.length; i++) {
-
-        if (typeof numbers[i] === "number") numbers[i] = numbers[i] + 1;
-    }
+    numbers = numbers.map(e => typeof e === "number" ? ++e : e)
 }
 
 incrementNumbers()
