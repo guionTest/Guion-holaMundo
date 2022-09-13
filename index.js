21b5bb0e8b931a9e567f2d701964cd5c05cc15fa
guionTest <113214406+guionTest@users.noreply.github.com>
2022-09-13 08:54:38 -0400

    Mejora en incrementNumbers

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-let numbers = [1, 2, 3, 4, "holaMundo", 5];
+let numbers = [1, 2, 3, 4, 'holaMundo', 5];
 
 //Incrementa los numeros de un array por 1
 function incrementNumbers() {
-    for (let i = 0; i < numbers.length; i++) {
-      typeof numbers[i] === "number" ? numbers[i] += 1 : null
-    }
+  numbers = numbers.map(e => (typeof e === 'number' ? e : null));
 }
 
-incrementNumbers()
+incrementNumbers();
 
-console.log(numbers)
+console.log(numbers);
